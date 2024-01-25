ded948d779fd746fbee515997bcd6e51bdd4abd5
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-01-25 00:36:09 -0500

    fix: swagger ui version cdn

--- src/apiDoc.js
+++ src/apiDoc.js
@@ -2,7 +2,7 @@ import swaggerUI from 'swagger-ui-express'
 import { readFile } from 'fs/promises'
 
 const CSS_URL =
-  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.7.0/swagger-ui.min.css'
 
 const swaggerJson = JSON.parse(await readFile(new URL('./swagger.json', import.meta.url)))
 
