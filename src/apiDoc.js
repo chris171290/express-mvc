7ab03530622140803cff0e05b89b5f7086895230
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-01-25 00:58:00 -0500

    style:

--- src/apiDoc.js
+++ src/apiDoc.js
@@ -1,8 +1,11 @@
 import swaggerUI from 'swagger-ui-express'
 import { readFile } from 'fs/promises'
 
-const CSS_URL =
-  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.7.0/swagger-ui.min.css'
+const CSS_URL = [
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.7.0/swagger-ui.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.7.0/swagger-ui.css'
+
+]
 
 const swaggerJson = JSON.parse(await readFile(new URL('./swagger.json', import.meta.url)))
 
